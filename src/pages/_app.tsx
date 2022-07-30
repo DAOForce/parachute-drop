f4be6755524605d2f85f210cc0b04916330ca76e
nahee-park <nahee.developer@gmail.com>
2022-07-31 08:47:03 +0900

    dev: react-query 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,10 @@ import { global } from '@src/styles/global';
 import theme from '@src/styles/theme';
 import type { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   const [isLogin, setIsLogin] = useState<string | null>('false');
   useEffect(() => {
     setIsLogin(localStorage.getItem('isLogin'));
@@ -17,7 +19,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Global styles={global} />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} isLogin={isLogin} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} isLogin={isLogin} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
