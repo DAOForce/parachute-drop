d0e586a4f4e967da188097e6699c05ff7f60d54d
snoopso <mingyeongso@mingyeongs-MacBook-Air.local>
2022-07-31 06:07:17 +0900

    라우트 작업

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,19 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { global } from '@src/styles/global';
 import theme from '@src/styles/theme';
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLogin, setIsLogin] = useState<string | null>('false');
+  useEffect(() => {
+    setIsLogin(localStorage.getItem('isLogin'));
+  }, [isLogin]);
+
   return (
     <>
       <Global styles={global} />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Component {...pageProps} isLogin={isLogin} />
       </ThemeProvider>
     </>
   );
