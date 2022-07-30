a3697c2d08eb9869b69b48b92bdcdd857de97fca
nahee-park <nahee.developer@gmail.com>
2022-07-31 00:32:12 +0900

    feat: 헤더 완성

--- src/pages/createspace/setdelegation.tsx
+++ src/pages/createspace/setdelegation.tsx
@@ -3,10 +3,13 @@ import styled from '@emotion/styled';
 import Title from '@src/components/CreateSpace/Title';
 import Subscription from '@src/components/CreateSpace/Subscription';
 import UploadCsv from '@src/components/common/UploadCsv';
+import CreateSpaceNav from '@src/components/common/CreateSpaceNav';
+import { HEADER_NAME } from '@src/constants';
 
 function setdelegation() {
   return (
     <StyleMain>
+      <CreateSpaceNav routingAddress="/">{HEADER_NAME.START_NEW_AIRDROP}</CreateSpaceNav>
       <Title>Set Delegation</Title>
       <Subscription>Check if you want to make user to delegate</Subscription>
       <UploadCsv />
