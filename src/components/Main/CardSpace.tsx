d08ab43f1017cc07e25549753e87520b51b7f329
snoopso <mingyeongso@mingyeongs-MacBook-Air.local>
2022-07-31 07:51:20 +0900

    API 연결 준비 완료

--- src/components/Main/CardSpace.tsx
+++ src/components/Main/CardSpace.tsx
@@ -2,14 +2,14 @@ import { useRouter } from 'next/router';
 
 interface CardSpaceProps {
   index: number;
-  profile: string;
-  name: string;
-  number: number;
-  id: string;
+  ticker: string;
+  intro: string;
+  DAOName: string;
+  image: string;
 }
 
 function CardSpace(props: CardSpaceProps) {
-  const { index, profile, name, number, id } = props;
+  const { index, ticker, intro, DAOName, image } = props;
 
   const router = useRouter();
 
@@ -23,14 +23,12 @@ function CardSpace(props: CardSpaceProps) {
   return (
     <div key={index} className="card shadow-xl bg-[#191919]">
       <div className="card-body items-center text-center mb-2">
-        <img src={profile} className="w-24 rounded-full" />
+        <img src={image} className="w-24 rounded-full" />
 
-        <h2 className="card-title">{name}</h2>
-        <p>
-          <span>{number}</span> members
-        </p>
+        <h2 className="card-title">{DAOName}</h2>
+        <p>{intro}</p>
         <div className="card-actions justify-center mt-2">
-          <button className="btn btn-warning" onClick={() => handleClick(id)}>
+          <button className="btn btn-warning" onClick={() => handleClick(ticker)}>
             Join
           </button>
         </div>
