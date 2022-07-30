4dbb80829dac438f4544b6b0ac1de8d18d5a953e
nahee-park <nahee.developer@gmail.com>
2022-07-31 05:36:45 +0900

    feat: review_airdrop 페이지 개발

--- src/pages/newairdrop/review_airdrop.tsx
+++ src/pages/newairdrop/review_airdrop.tsx
@@ -1,7 +1,116 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+import Title from '@src/components/CreateSpace/Title';
+import Subscription from '@src/components/CreateSpace/Subscription';
+import UploadCsv from '@src/components/common/UploadCsv';
+import CreateSpaceNav from '@src/components/common/CreateSpaceNav';
+import { HEADER_NAME, SUBSCRIPTION, TITLE } from '@src/constants';
+import BackBtn from '@src/components/common/BackBtn';
+import NextBtn from '@src/components/common/NextBtn';
+import { useRouter } from 'next/router';
+import { FreeObject } from '@src/types';
 
 function review_airdrop() {
-  return <div>review_airdrop</div>;
+  const router = useRouter();
+  const handleNextClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    router.push({
+      pathname: '/claimtoken',
+      query: {
+        ...router.query,
+      },
+    });
+  };
+  const handleBackClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    router.push({
+      pathname: '/newairdrop/add_whitelist_address',
+      query: {
+        ...router.query,
+      },
+    });
+  };
+
+  return (
+    <StyleMain>
+      <CreateSpaceNav routingAddress="/">{HEADER_NAME.START_NEW_AIRDROP}</CreateSpaceNav>
+      <Title>{TITLE.REVIEW_AIRDROP}</Title>
+      <StyledRecipt>
+        <h2>Treasury address</h2>
+        <p>{router?.query?.treasuryAddress}</p>
+        <h2>Rounds</h2>
+        <p>{router?.query?.rounds} times</p>
+        <h2>Interval</h2>
+        <p>{router?.query?.interval} days</p>
+        <h2>Claimable Duration</h2>
+        <p>{router?.query?.duration} weeks</p>
+      </StyledRecipt>
+      <StyledButtonGroup>
+        <BackBtn onClick={handleBackClick} />
+        <NextBtn onClick={handleNextClick} />
+      </StyledButtonGroup>
+    </StyleMain>
+  );
 }
 
 export default review_airdrop;
+
+const StyleMain = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  & > header {
+    margin-bottom: 120px;
+  }
+  & > h1 {
+    margin-bottom: 76px;
+  }
+`;
+
+const StyledRecipt = styled.section`
+  width: 512px;
+  height: 604px;
+  background: rgba(255, 255, 255, 0.1);
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  & h2 {
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 145%;
+
+    letter-spacing: 0.005em;
+
+    color: #f5f5f5;
+    margin-bottom: 10px;
+  }
+
+  & p {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 48px;
+
+    color: #ffffff;
+    margin-bottom: 48px;
+    &:last-child {
+      margin-bottom: 0px;
+    }
+  }
+`;
+
+const StyledButtonGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 50px;
+  margin-bottom: 150px;
+  width: 582px;
+`;
