55162f33e9a32457631e51e458ff29534d2c5d95
nahee-park <nahee.developer@gmail.com>
2022-09-21 04:09:29 +0900

    feat: portfolio 더미

--- src/components/Detail/DetailPortfolio.tsx
+++ src/components/Detail/DetailPortfolio.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import IBC from '@src/assets/Icon/IBC.svg';
+import Title from '../CreateSpace/Title';
 
 function DetailPortfolio() {
-  return <div>DetailPortfolio</div>;
+  return (
+    <section className="flex flex-col justify-center items-center">
+      <div className="flex flex-row justify-between w-[98%] mt-[37.5px] mb-[32.5px]">
+        <Title>My Tokens</Title>
+        <Title>1,000.00 TEL</Title>
+      </div>
+      <IBC className="mb-[106px]" />
+    </section>
+  );
 }
 
 export default DetailPortfolio;
